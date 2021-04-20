49463ba9d6e4014c090f46a019a296d9d8afb460
puja bhandari <puzabhandari74.20@gmail.com>
2021-04-20 19:54:48 +1000

    layout created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {Button} from 'react-bootstrap'
 import './App.css';
+import { DefaultLayout } from './layout/DefaultLayout';
 import { Entry } from './pages/entry/Entry.page';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Entry />
+       {/* <Entry />*/}
+        <DefaultLayout >
+          send page components
+        </DefaultLayout>
       
       </header>
     </div>
